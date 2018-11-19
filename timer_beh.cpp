28f822d6d6726fd363b1974f99840dbe93ad9bf3
David C Black <david.black@doulos.com>
2018-11-18 20:54:37 -0600

    Tested one-shot and reload behaviors

--- timer_beh.cpp
+++ timer_beh.cpp
@@ -4,40 +4,43 @@
 //   This implements the behavior of a generic timer function.
 
 #include "timer_beh.hpp"
+#include "sc_cxx11.hpp"
 #include "report.hpp"
 
 using namespace sc_core;
-
-// Class static constants
-const char* Timer::MSGID { "/Doulos/example/timer_beh" };
+namespace {
+  const char* const MSGID { "/Doulos/example/timer_beh" };
+}
 
 //------------------------------------------------------------------------------
 // Constructor
 Timer::Timer( sc_module_name nm )
   : sc_module( nm )
+  , m_pulse_delay(1_ps)
 {
   SC_HAS_PROCESS( Timer );
   SC_THREAD( trigger_thread );
-  INFO( MEDIUM, "Default constructed " << __func__ );
+  INFO( MEDIUM, "Constructed " << name() );
 }
 
 //------------------------------------------------------------------------------
 // Destructor
 Timer::~Timer( void )
 {
-  INFO( MEDIUM, "Destroyed " << __func__ );
+  INFO( MEDIUM, "Destroyed " << name() );
 }
 
 //------------------------------------------------------------------------------
 void Timer::start( sc_time delay )
 {
-  if ( m_paused ) {
+  INFO( DEBUG, "Starting timer " << name() );
+  if( m_paused ) {
     resume();
   }
   else {
     m_start_time = curr_time( delay );
-    m_trigger_time = m_start_time + m_trig_delay;
-    m_trigger_event.notify( m_trig_delay + delay );
+    m_trigger_time = m_start_time + m_load_delay;
+    m_trigger_event.notify( m_load_delay + delay );
     INFO( DEBUG, "Timer " << name() << " started." );
   }
 }
@@ -45,6 +48,7 @@ void Timer::start( sc_time delay )
 //------------------------------------------------------------------------------
 void Timer::stop( sc_time delay )
 {
+  INFO( DEBUG, "Stopping timer " << name() );
   // Cancel outstanding event
   m_trigger_event.cancel();
   m_trigger_time = SC_ZERO_TIME;
@@ -57,6 +61,7 @@ void Timer::stop( sc_time delay )
 //------------------------------------------------------------------------------
 void Timer::pause( sc_time delay )
 {
+  INFO( DEBUG, "Pausing timer " << name() );
   m_paused = true;
   m_trigger_event.cancel();
   m_resume_delay = m_trigger_time - curr_time( delay );
@@ -66,10 +71,11 @@ void Timer::pause( sc_time delay )
 //------------------------------------------------------------------------------
 void Timer::resume( sc_time delay )
 {
+  INFO( DEBUG, "Resuming timer " << name() );
   sc_assert( m_paused and m_resume_delay != SC_ZERO_TIME );
   m_paused = false;
   m_trigger_time = curr_time( delay ) + m_resume_delay;
-  m_start_time = m_trigger_time - m_trig_delay;
+  m_start_time = m_trigger_time - m_load_delay;
   m_trigger_event.notify( m_resume_delay + delay );
   m_resume_delay = SC_ZERO_TIME;
   INFO( DEBUG, "Timer " << name() << " resumed." );
@@ -85,8 +91,20 @@ sc_time Timer::curr_time( sc_time delay ) const
 // To find out how much time is left on the clock
 sc_time Timer::get_time_left( sc_time delay ) const
 {
-  if ( m_trigger_time > curr_time(delay) ) { //< is it in the future?
+  if( m_trigger_time > curr_time(delay) ) { //< is it in the future?
     return ( m_trigger_time - curr_time(delay) ); //< amount of time left
+  }
+  else {
+    return SC_ZERO_TIME; //< no time left
+  }
+}
+
+//------------------------------------------------------------------------------
+// To find out how much time since timer started
+sc_time Timer::get_time_since( sc_time delay ) const
+{
+  if( m_start_time > SC_ZERO_TIME and m_start_time < curr_time(delay) ) {
+    return curr_time(delay) - m_start_time;
   }
   else {
     return SC_ZERO_TIME; //< no time left
@@ -98,30 +116,251 @@ bool Timer::is_running( sc_core::sc_time delay )
 {
   bool running = not m_paused;
   running &= ( m_trigger_time != SC_ZERO_TIME );
-  running &= ( get_time_left( delay ) == SC_ZERO_TIME ) and not m_continuous;
+  running &= ( get_time_left( delay ) > SC_ZERO_TIME ) or m_continuous;
   return running;
 }
 
 //------------------------------------------------------------------------------
 void Timer::trigger_thread( void )
 {
-  wait( m_trigger_event );
-  sc_assert ( m_trigger_time != SC_ZERO_TIME
-       and m_trigger_time == sc_time_stamp()
-       and not m_paused );
+  for(;;) {
+    wait( m_trigger_event );
+    sc_assert ( m_trigger_time != SC_ZERO_TIME
+         and m_trigger_time == sc_time_stamp()
+         and not m_paused );
 
-  m_triggered = true;
+    m_triggered = true;
+    if( m_pulse_delay > SC_ZERO_TIME ) {
+      m_pulse_event.notify( m_pulse_delay );
+    }
 
-  if ( m_reload and m_trig_delay != SC_ZERO_TIME ) {
-    m_start_time = curr_time( );
-  }
+    if( m_reload and m_load_delay != SC_ZERO_TIME ) {
+      m_start_time = sc_time_stamp( );
+    }
 
-  if ( m_continuous ) {
-    m_trigger_time = curr_time( ) + m_trig_delay;
-    m_trigger_event.notify( m_trig_delay );
-  } else {
-    m_trigger_time = SC_ZERO_TIME;
+    if( m_continuous ) {
+      m_trigger_time = sc_time_stamp( ) + m_load_delay;
+      m_trigger_event.notify( m_load_delay );
+    } else {
+      m_trigger_time = SC_ZERO_TIME;
+    }
   }
 }
 
-//EOF
+//------------------------------------------------------------------------------
+std::string Timer::to_string( void )
+{
+  std::ostringstream os;
+  os << "timer state: {\n"
+     << "-  current time: " << sc_time_stamp() << "\n"
+     << "-    load delay: " << get_load_delay() << "\n"
+     << "-   pulse delay: " << get_pulse_delay() << "\n"
+     << "-    start time: " << get_start_time() << "\n"
+     << "-  trigger time: " << get_trigger_time() << "\n"
+     << "-     time left: " << get_time_left() << "\n"
+     << "-    time since: " << get_time_since() << "\n"
+     << "-  trigger flag: " << (get_triggered()?"TRIGGERED":"-") << "\n"
+     << "-     mode flag: " << (get_continuous()?"CONTINUOUS":"one-shot") << "\n"
+     << "-   reload flag: " << (get_reload()?"ACTIVE":"off") << "\n"
+     << "- running state: " << (is_running()?"RUNNING":"off") << "\n"
+     << "-  paused state: " << (is_paused()?"PAUSED":"-") << "\n"
+     << "}" << std::ends
+  ;
+  return os.str();
+}
+
+//------------------------------------------------------------------------------
+void Timer::reset( void ) {
+  m_trigger_event.cancel();
+  m_pulse_event.cancel();
+  m_load_delay   = sc_core::SC_ZERO_TIME;
+  m_start_time   = sc_core::SC_ZERO_TIME;
+  m_pulse_delay  = sc_core::SC_ZERO_TIME;
+  m_resume_delay = sc_core::SC_ZERO_TIME;
+  m_trigger_time = sc_core::SC_ZERO_TIME;
+  m_triggered    = false;
+  m_reload       = false;
+  m_continuous   = false;
+  m_paused       = false;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  ##### #     #    #    #     # #####  #     #####                             
+//  #      #   #    # #   ##   ## #    # #     #                                 
+//  #       # #    #   #  # # # # #    # #     #                                 
+//  #####    #    #     # #  #  # #####  #     #####                             
+//  #       # #   ####### #     # #      #     #                                 
+//  #      #   #  #     # #     # #      #     #                                 
+//  ##### #     # #     # #     # #      ##### #####                             
+//
+////////////////////////////////////////////////////////////////////////////////
+#ifdef TIMER_EXAMPLE
+#include "timer_beh.hpp"
+#include "report.hpp"
+#include "common.hpp"
+#include "summary.hpp"
+#include "no_clock.hpp"
+#include <queue>
+#include <iostream>
+std::ostringstream mout;
+using namespace std;
+
+//------------------------------------------------------------------------------
+SC_MODULE( Top_module )
+{
+  const char* MSGID{ "/Doulos/Example/Timer_example" };
+  no_clock clk   { "clk", 10_ns };
+
+  //----------------------------------------------------------------------------
+  void test_thread( void ) {
+    // Display reset state
+    MESSAGE("\n");
+    RULER('R');
+    INFO( MEDIUM, "Reset " << timer.to_string() );
+
+    // Test one shot
+    MESSAGE("\n");
+    RULER('1');
+    clk.wait_posedge(1);
+    timer.set_load_delay( 10_ns );
+    timer.set_one_shot();
+    timer.set_reload( false );
+    INFO( MEDIUM, "One-shot setup " << timer.to_string() );
+    expect.push( sc_time_stamp() + 10_ns );
+    timer.start( SC_ZERO_TIME );
+    wait(5_ns);
+    INFO( MEDIUM, "One-shot midway " << timer.to_string() );
+    wait(6_ns);
+    INFO( MEDIUM, "One-shot finished " << timer.to_string() );
+
+    // Test one shot with 1ns pause
+    MESSAGE("\n");
+    RULER('P');
+    clk.wait_posedge();
+    timer.set_load_delay( 10_ns );
+    timer.set_one_shot();
+    timer.set_reload( false );
+    INFO( MEDIUM, "One-shot setup " << timer.to_string() );
+    expect.push( sc_time_stamp() + 21_ns );
+    timer.start( SC_ZERO_TIME );
+    wait(5_ns);
+    INFO( MEDIUM, "One-shot midway " << timer.to_string() );
+    timer.pause();
+    INFO( MEDIUM, "One-shot paused " << timer.to_string() );
+    wait(11_ns);
+    INFO( MEDIUM, "One-shot resuming " << timer.to_string() );
+    timer.resume();
+    wait(6_ns);
+    INFO( MEDIUM, "One-shot finished " << timer.to_string() );
+
+    // Test re-load
+    MESSAGE("\n");
+    RULER('L');
+    clk.wait_posedge();
+    timer.set_load_delay( 5_ns );
+    timer.set_continuous();
+    timer.set_reload( true );
+    expect.push( sc_time_stamp() +  5_ns );
+    expect.push( sc_time_stamp() + 10_ns );
+    expect.push( sc_time_stamp() + 15_ns );
+    timer.start( SC_ZERO_TIME );
+    INFO( MEDIUM, "Reload setup " << timer.to_string() );
+    wait(3_ns);
+    INFO( MEDIUM, "Reload running " << timer.to_string() );
+    wait(5_ns);
+    INFO( MEDIUM, "Reload running " << timer.to_string() );
+    wait(10_ns);
+    INFO( MEDIUM, "Reload finished " << timer.to_string() );
+
+    // Test reset
+    timer.reset();
+    INFO( MEDIUM, "Reset " << timer.to_string() );
+
+    while ( not expect.empty() ) {
+      REPORT( ERROR, "Missed event for " << expect.top() );
+      expect.pop();
+      ++missed_count;
+    }
+    sc_stop();
+  }
+
+  //----------------------------------------------------------------------------
+  void interrupt_thread( void ) {
+    for(;;) {
+      INFO( MEDIUM, "Waiting for timeout at " << sc_time_stamp() );
+      wait( timer.trigger_event() );
+      ++event_count;
+      if( expect.empty() ) {
+        REPORT( ERROR, "Unexpected event at " << sc_time_stamp() );
+        ++unexpected_count;
+      }
+      else if( expect.top() == sc_time_stamp() ) {
+        INFO( MEDIUM, "Expected event at " << sc_time_stamp() );
+        expect.pop();
+        ++expected_count;
+      }
+      else if( expect.top() < sc_time_stamp() ) {
+        while ( not expect.empty() and expect.top() < sc_time_stamp() ) {
+          REPORT( ERROR, "Missed event for " << expect.top() );
+          expect.pop();
+          ++missed_count;
+        }
+        if( expect.top() == sc_time_stamp() ) {
+          INFO( MEDIUM, "Expected event at " << sc_time_stamp() );
+          expect.pop();
+          ++expected_count;
+        }
+        else {
+          REPORT( ERROR, "Unexpected event at " << sc_time_stamp() );
+          ++unexpected_count;
+        }
+      }
+      else if( expect.top() > sc_time_stamp() ) {
+        REPORT( ERROR, "Unexpected event at " << sc_time_stamp() );
+        ++unexpected_count;
+      }
+    }
+  }
+
+  //----------------------------------------------------------------------------
+  SC_CTOR( Top_module ) {
+    SC_THREAD( test_thread );
+    SC_THREAD( interrupt_thread );
+  }
+
+  //----------------------------------------------------------------------------
+  const char* kind() const override {
+    return "Top_module";
+  }
+  // Attributes
+  Timer timer{"timer"};
+  size_t event_count      { 0 };
+  size_t missed_count     { 0 };
+  size_t expected_count   { 0 };
+  size_t unexpected_count { 0 };
+  std::priority_queue<sc_time, std::vector<sc_time>, std::greater<sc_time>> expect;
+};
+
+//------------------------------------------------------------------------------
+int sc_main( int argc, char* argv[] )
+{
+  for ( int i = 1; i < sc_core::sc_argc(); ++i ) {
+    std::string arg( sc_core::sc_argv()[i] );
+    if( arg == "-debug" ) {
+      sc_core::sc_report_handler::set_verbosity_level( SC_DEBUG );
+      SC_REPORT_INFO( "/Doulos/example/timer-test", "Verbosity level set to DEBUG" );
+    }
+  }
+  sc_report_handler::set_actions( SC_ERROR, SC_DISPLAY | SC_LOG );
+  Summary::starting_elaboration();
+  Top_module top( "top" );
+  Summary::starting_simulation(); //< best we can do
+  sc_start();
+  Summary::finished_simulation();
+  return Summary::report();
+}
+#endif
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2018 by Doulos. All rights reserved.
+//END $Id$
--- timer_beh.hpp
+++ timer_beh.hpp
@@ -2,64 +2,84 @@
 #define TIMER_BEH_HPP
 
 #include <systemc>
+#include <string>
 
 struct Timer
-  : sc_core::sc_module {
-  Timer( sc_core::sc_module_name nm );   //< Constructor
+: sc_core::sc_module
+{
+  using sc_module_name = sc_core::sc_module_name;
+  using sc_time        = sc_core::sc_time;
+  using sc_event       = sc_core::sc_event;
+
+  Timer( sc_module_name nm );   //< Constructor
   ~Timer( void );                        //< Destructor
 
   // Processes
   void trigger_thread( void );
 
   // Inline accessors
-  void set_trig_delay( sc_core::sc_time t ) { m_trig_delay = t; }
-  void set_start_time( sc_core::sc_time t ) { m_start_time = t; }
-  void set_pulse_time( sc_core::sc_time t ) { m_pulse_time = t; }
-  void set_trigger_time( sc_core::sc_time t ) { m_trigger_time = t; }
-  sc_core::sc_time get_trig_delay( void ) const { return m_trig_delay; }
-  sc_core::sc_time get_start_time( void ) const { return m_start_time; }
-  sc_core::sc_time get_pulse_time( void ) const { return m_pulse_time; }
-  sc_core::sc_time get_trigger_time( void ) const { return m_trigger_time; }
-  void set_triggered( bool v ) { m_triggered = v; }
-  void set_reload( bool v ) { m_reload = v; }
-  void set_continuous( bool v ) { m_continuous = v; }
-  bool get_triggered( void ) const { return m_triggered; }
-  bool get_reload( void ) const { return m_reload; }
-  bool get_continuous( void ) const { return m_continuous; }
-  bool is_paused( void ) const { return m_paused; }
+  void    set_load_delay  ( sc_time t )  { m_load_delay   = t;    }
+  //id    set_start_time  ( sc_time t )  { m_start_time   = t;    }
+  void    set_pulse_delay ( sc_time t )  { m_pulse_delay  = t;    }
+  //id    set_trigger_time( sc_time t )  { m_trigger_time = t;    }
+  sc_time get_load_delay  ( void ) const { return m_load_delay;   }
+  sc_time get_start_time  ( void ) const { return m_start_time;   }
+  sc_time get_pulse_delay ( void ) const { return m_pulse_delay;  }
+  sc_time get_trigger_time( void ) const { return m_trigger_time; }
+  void    set_triggered   ( bool v=true) { m_triggered  = v;      }
+  void    clr_triggered   ( void )       { m_triggered  = false;  }
+  void    set_reload      ( bool v=true) { m_reload     = v;      }
+  void    set_continuous  ( bool v=true) { m_continuous = v;      }
+  void    set_one_shot    ( void )       { m_continuous = false;  }
+  bool    get_triggered   ( void ) const { return m_triggered;    }
+  bool    get_reload      ( void ) const { return m_reload;       }
+  bool    get_continuous  ( void ) const { return m_continuous;   }
+  bool    is_paused       ( void ) const { return m_paused;       }
 
   // Fundamental behaviors of model
-  sc_core::sc_time curr_time( sc_core::sc_time delay = sc_core::SC_ZERO_TIME ) const;
-  void start ( sc_core::sc_time delay = sc_core::SC_ZERO_TIME );
-  void stop  ( sc_core::sc_time delay = sc_core::SC_ZERO_TIME );
-  void pause ( sc_core::sc_time delay = sc_core::SC_ZERO_TIME );
-  void resume( sc_core::sc_time delay = sc_core::SC_ZERO_TIME );
-  const sc_core::sc_event& trigger_event( void ) const
+  sc_time         curr_time    ( sc_time delay = sc_core::SC_ZERO_TIME ) const;
+  void            start        ( sc_time delay = sc_core::SC_ZERO_TIME );
+  void            stop         ( sc_time delay = sc_core::SC_ZERO_TIME );
+  void            pause        ( sc_time delay = sc_core::SC_ZERO_TIME );
+  void            resume       ( sc_time delay = sc_core::SC_ZERO_TIME );
+  const sc_event& trigger_event( void ) const
   {
     return m_trigger_event;  //< triggered when timer expires
   }
-
-  sc_core::sc_time get_time_left( sc_core::sc_time delay = sc_core::SC_ZERO_TIME ) const;  //< how much time remains
-  bool is_running( sc_core::sc_time delay = sc_core::SC_ZERO_TIME );
-
-  bool triggered( void ) //< indicates if trigger event was noted, but only does so once per trigger
+  const sc_event& pulse_event( void ) const
   {
-    bool result = m_triggered;
-    m_triggered = false;
-    return result;
+    return m_pulse_event;  //< triggered when pulse time completes
   }
 
+  // how much time remains until next trigger
+  sc_time get_time_left( sc_time delay = sc_core::SC_ZERO_TIME ) const;
+  sc_time get_time_since( sc_time delay = sc_core::SC_ZERO_TIME ) const;
+  bool    is_running   ( sc_time delay = sc_core::SC_ZERO_TIME );
+
+  // Indicate if trigger event was noted, but only do so once per trigger
+  bool test_and_clear_triggered( void )
+  {
+    bool result = get_triggered();
+    clr_triggered();
+    return result;
+  }
+  std::string to_string( void );
+  void reset( void );
+
 private:
-  sc_core::sc_time   m_trig_delay   { sc_core::SC_ZERO_TIME }; // trigger_time - start_time
-  sc_core::sc_time   m_start_time   { sc_core::SC_ZERO_TIME }; // when timer started
-  sc_core::sc_time   m_pulse_time   { sc_core::SC_ZERO_TIME }; // nominally one clock period, but could change for PWM
-  sc_core::sc_time   m_resume_delay { sc_core::SC_ZERO_TIME };
-  sc_core::sc_time   m_trigger_time { sc_core::SC_ZERO_TIME }; //< start_time + trig_delay // (0 => stopped)
-  bool               m_triggered    { false }; //< indicates timer has triggered
-  bool               m_reload       { false }; // set to cause automatic retrigger computation on trigger
-  bool               m_continuous   { false }; // set to keep timer going after trigger
-  bool               m_paused       { false };
-  sc_core::sc_event  m_trigger_event;
-  static const char* MSGID;
+  // Terminology:
+  //   _time implies absolute time (sc_time_stamp())
+  //   _delay is relative time
+  sc_time   m_load_delay   { sc_core::SC_ZERO_TIME }; //< trigger_time - start_time
+  sc_time   m_start_time   { sc_core::SC_ZERO_TIME }; //< when timer started
+  sc_time   m_pulse_delay  { sc_core::SC_ZERO_TIME }; //< nominally one clock period, but could change for PWM
+  sc_time   m_resume_delay { sc_core::SC_ZERO_TIME }; //< time left on clock when paused
+  sc_time   m_trigger_time { sc_core::SC_ZERO_TIME }; //< absolute time when timeout happens = start_time + load_delay // (0 => stopped)
+  bool      m_triggered    { false }; //< indicates timer has triggered
+  bool      m_reload       { false }; //< set to cause automatic retrigger computation on trigger
+  bool      m_continuous   { false }; //< set to keep timer going after trigger
+  bool      m_paused       { false }; //< indicates if paused
+  sc_event  m_trigger_event;
+  sc_event  m_pulse_event;
 };
 #endif /* TIMER_BEH_HPP */
