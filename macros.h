9f47221cfe82bd6c6a42d01a2dafe8240c58ea73
David C Black <david.black@doulos.com>
2018-12-30 15:56:21 -0600

    Add more practical macros for setting/testing. May consider different approach later.

--- macros.h
+++ macros.h
@@ -4,5 +4,8 @@
 #define BIT(n) (1ul<<(n))      /* 0 <= n < 32 */
 #define BITS(w) ((1ul<<(w))-1) /* 0 <  w < 32 */
 #define MASK(reg) (BITS(reg##_BITS)<<reg##_LSB)
+#define SET_FIELD(reg,var,val) var=(var&~reg##_MASK)|((val<<reg##_LSB)&reg##_MASK)
+#define GET_FIELD(reg,val) ((var&reg##_MASK)>>reg##_LSB)
+#define IS_NONZERO(reg,val) ((var&reg##_MASK) == 0)
 
 #endif /*MACROS_H*/
