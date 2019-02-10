049529110d8529fcd01bcb612e488fbc269c5c4b
David C Black <david.black@doulos.com>
2019-02-10 06:53:51 -0600

    Move implemetation

--- tcpip/tcpip_tx.hpp
+++ tcpip/tcpip_tx.hpp
@@ -4,6 +4,13 @@
 #include "tcpip/tcpip_tx_if.hpp"
 #include <systemc>
 #include <thread>
+#include <strings.h>
+#include <cstring>
+#include <sys/socket.h>
+#include <sys/errno.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
 
 template<typename T>
 struct Tcpip_tx_channel
@@ -11,7 +18,8 @@ struct Tcpip_tx_channel
 , Tcpip_tx_if
 {
   // Constructor
-  Tcpip_tx_channel( sc_core::sc_module_name instance_name )
+  Tcpip_tx_channel( sc_core::sc_module_name instance_name, uint32_t port )
+  : m_tx_port( port )
   {
   }
 
@@ -23,23 +31,137 @@ struct Tcpip_tx_channel
   void start_of_simulation()
   {
     // Launch thread
+    m_tx_osthread = std::thread( tx_osthread );
   }
 
   // Processes
   void tx_osthread( void ) //< not SystemC
   {
+    //--------------------------------------------------------------------------
     // Create TCP/IP socket
-    // Bind to incoming address
-    // Listen for a connection
-    // Accept connection
-    // Accept connection
-    for(;;) {
-      // Wait for data from SystemC locally
-      tx.wait_unless_available();
-      // Packetize data
-      // Send packet to remote
-      // If shutdown, exit
+    //--------------------------------------------------------------------------
+    int listening_socket = socket( AF_INET, SOCK_STREAM, 0 );
+    if ( listening_socket == -1 ) {
+      REPORT( FATAL, "Could not create socket" );
     }
+
+    int option_value = 1;
+    if ( setsockopt
+         ( listening_socket
+         , SOL_SOCKET
+         , SO_REUSEADDR
+         , ( void* )&option_value
+         , ( socklen_t )( sizeof( socklen_t ) )
+         ) < 0
+       )
+    {
+      REPORT( FATAL, "Unable to set socket option" );
+    }
+
+    //--------------------------------------------------------------------------
+    // Prepare the server address sockaddr_in structure
+    //--------------------------------------------------------------------------
+    struct sockaddr_in local_server;
+    bzero( ( char* ) &local_server, sizeof( local_server ) );
+    local_server.sin_family = AF_INET;
+    local_server.sin_addr.s_addr = INADDR_ANY;
+    local_server.sin_port = htons( m_tcpip_port ); // Host to Network -- big-endian conversion
+
+    //--------------------------------------------------------------------------
+    // Bind to incoming address in preparation to listening
+    //--------------------------------------------------------------------------
+    if ( 0 != ::bind
+                ( listening_socket
+                , ( struct sockaddr * )&local_server
+                , sizeof( local_server )
+                )
+       )
+    {
+      REPORT( FATAL, "Bind failed" );
+      sc_core::sc_abort();
+    }
+    REPORT( NOTE, "Bind done" );
+
+    accept_loop: for(;;) {
+      //------------------------------------------------------------------------
+      // Listen for a connection
+      //------------------------------------------------------------------------
+      REPORT( INFO, "Queueing incoming connections..." );
+      listen( listening_socket, 1 /*request at a time*/ );
+
+      //------------------------------------------------------------------------
+      // Accept connection
+      //------------------------------------------------------------------------
+      REPORT( INFO, "Waiting for incoming connections..." );
+      // Accept an incoming connection
+      int addr_len = sizeof( struct sockaddr_in );
+      struct sockaddr_in remote_client;
+      bzero( ( char* ) &remote_client, sizeof( local_server ) );
+      incoming_socket = accept( listening_socket
+                              , ( struct sockaddr * )&remote_client
+                              , ( socklen_t * )&addr_len
+                              );
+      if ( incoming_socket < 0 ) {
+        REPORT( FATAL, "Accept failed: " << strerror( errno ) );
+      }
+      REPORT( NOTE, "Connection accepted..." );
+      //------------------------------------------------------------------------
+      // Check for valid requestor
+      //------------------------------------------------------------------------
+      {
+        std::unique_ptr<char*> recv_buffer{ new char[256] };
+        bzero( recv_buffer.get(), 256 );
+        n = read( sockfd, recv_buffer.get(), 255 );
+        if ( n < 0 ) {
+          REPORT( ERROR, "ERROR reading from socket" );
+        }
+        if ( SECRET != recv_buffer.get() ) {
+          continue;
+        }
+        REPORT( NOTE, "Password accepted..." );
+      }
+
+      //------------------------------------------------------------------------
+      // Begin receiving and transmitting data
+      //------------------------------------------------------------------------
+      server_loop: for(;;) {
+        //----------------------------------------------------------------------
+        // Wait for data from SystemC locally
+        //----------------------------------------------------------------------
+        tx.wait_unless_available();
+
+        //----------------------------------------------------------------------
+        // Exit if commanded
+        //----------------------------------------------------------------------
+        if ( tlmx_trans_ptr->command == TLMX_EXIT ) {
+          REPORT( NOTE, "Exiting due to TLMX_EXIT..." );
+          break;
+        }
+
+        //----------------------------------------------------------------------
+        // Packetize data
+        //----------------------------------------------------------------------
+        char transmit_buffer[TLMX_MAX_BUFFER];
+        bzero( transmit_buffer, TLMX_MAX_BUFFER );
+        int packed_size = tlmx_trans_ptr->pack( transmit_buffer );
+        sc_core::sc_assert( packed_size == unpacked_size );
+
+        //----------------------------------------------------------------------
+        // Send packet to remote
+        //----------------------------------------------------------------------
+        REPORT( NOTE, "Sending..." );
+        int send_count = write( incoming_socket, transmit_buffer, packed_size );
+        if( send_count < 0 ) {
+          REPORT( ERROR, "TCPIP write/send failed" << strerror( errno ) );
+        }
+        sc_core::sc_assert( send_count == packed_size );
+
+      }//endforever server_loop
+    }//endforever accept_loop
+
+    //------------------------------------------------------------------------
+    // If shutdown, exit
+    //------------------------------------------------------------------------
   }
 
   void end_of_simulation( void )
@@ -51,17 +173,20 @@ struct Tcpip_tx_channel
   // Methods
   virtual bool can_put( void ) const override
   {
+    return m_ready;
   }
 
   virtual const sc_core::sc_event& ok_to_put( void ) const override
   {
+    return m_ready_event;
   }
 
-  virtual void put( Async_payload& the_payload ) override
+  virtual void put( const Async_payload& the_payload ) override
   {
+    while ( not nb_put( the_payload ) ) wait( m_ready_event );
   }
 
-  virtual bool nb_put( Async_payload& the_payload ) override
+  virtual bool nb_put( const Async_payload& the_payload ) override
   {
   }
 
@@ -74,9 +199,12 @@ private:
   // Attributes
   uint32_t    m_tx_port;
   std::thread m_tx_osthread;
+  std::mutex  m_tx_mutex;
+  sc_core::sc_event m_ready_event;
+  bool        m_ready{ false };
 
-  static constexpr const char* const MSGID
-  { "/Doulos/{:$PROJECT:}/Tcpip_tx" };
+  static constexpr const std::string SECRET { "Mellon a minno" };// Speak "Friend and enter"
+  static constexpr const char* const MSGID  { "/Doulos/{:$PROJECT:}/Tcpip_tx" };
 };//end Tcpip_tx_channel
 
 #endif/*TCPIP_TX_HPP*/
